23ba36a015d82bb84d516a9e9687edda7ab7504a
sai wanza <q020385791@gmail.com>
2021-06-11 15:50:24 +0800

    加入專案檔案。

---
+++ OracleReader/ConDB.cs
@@ -0,0 +1,14 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleReader
+{
+    public class ConDB
+    {
+        public static OracleConnection db = new OracleConnection();
+    }
+}
---
+++ OracleReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+
+namespace OracleReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConDB.db.ConnectionString = "Data Source = (DESCRIPTION = (ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = '')(PORT = '')))(CONNECT_DATA =(SERVICE_NAME ='' ))); Persist Security Info = True; User ID ='' ; Password ='' ;";
+            ConDB.db.Open();
+            int UserIDCount = 0;
+            string sql = "";
+            using (OracleDataReader reader = new OracleCommand(sql, ConDB.db).ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    //select Count(Columnname) as EmpCount from Table
+                    UserIDCount = int.Parse(reader["EmpCount"].ToString());
+                }
+            }
+            ConDB.db.Close();
+          
+        }
+    }
+}
